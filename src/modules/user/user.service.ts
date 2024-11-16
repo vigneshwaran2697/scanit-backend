a2a662ce05facd2fa1888b22ebca51f45072f50e
vravic926_comcast <Vigneshwaran_Ravichandran@comcast.com>
2024-11-16 20:02:57 +0530

    getUserByEmail function modifiec

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -12,103 +12,93 @@ const SUPERADMIN_LAST_NAME = 'SuperAdmin';
 
 @Injectable()
 export class UserService {
-    constructor(
-        private readonly cognitoService:CognitoService,
-        private readonly userRepo: UserRepository,
-    ) {}
+  constructor(
+    private readonly cognitoService: CognitoService,
+    private readonly userRepo: UserRepository,
+  ) {}
 
-    public async createSuperAdminUser() {
-        const adminEmailId = config.appConfig.superAdminEmailId;
-        let adminUser = await this.userRepo.findOneByCond({
+  public async createSuperAdminUser() {
+    const adminEmailId = config.appConfig.superAdminEmailId;
+    let adminUser = await this.userRepo.findOneByCond({
+      emailId: adminEmailId,
+    });
+    if (!adminUser) {
+      let cognitoResp, username;
+      try {
+        cognitoResp = await this.cognitoService.createUserInCognito({
           emailId: adminEmailId,
+          password: config.appConfig.superAdminPassword,
+          userAttributes: [
+            { Name: 'email', Value: adminEmailId },
+            {
+              Name: 'name',
+              Value: `${SUPERADMIN_FIRST_NAME} ${SUPERADMIN_LAST_NAME}`,
+            },
+            { Name: 'custom:firstname', Value: SUPERADMIN_FIRST_NAME },
+            { Name: 'custom:lastname', Value: SUPERADMIN_LAST_NAME },
+          ],
         });
-        if (!adminUser) {
-          let cognitoResp, username;
-          try {
-            cognitoResp = await this.cognitoService.createUserInCognito({
-              emailId: adminEmailId,
-              password: config.appConfig.superAdminPassword,
-              userAttributes: [
-                { Name: 'email', Value: adminEmailId },
-                {
-                  Name: 'name',
-                  Value: `${SUPERADMIN_FIRST_NAME} ${SUPERADMIN_LAST_NAME}`,
-                },
-                { Name: 'custom:firstname', Value: SUPERADMIN_FIRST_NAME },
-                { Name: 'custom:lastname', Value: SUPERADMIN_LAST_NAME },
-              ],
-            });
-            username = cognitoResp?.UserSub;
-          } catch (error) {
-            console.log('Cognito Super Admin User Creation Error: ', error);
-            try {
-              cognitoResp = await this.cognitoService.getUserFromCognito(adminEmailId);
-              username = cognitoResp.Username;
-            } catch (err) {
-              console.log('Cognito Super Admin User Fetch Error: ', err);
-            }
-          }
-          if (username) {
-            const userInput: any = {
-              username,
-              emailId: adminEmailId,
-              firstName: SUPERADMIN_FIRST_NAME,
-              lastName: SUPERADMIN_LAST_NAME,
-              isActive: true,
-              userRole: UserRole.SUPER_ADMIN,
-            };
-            adminUser = await this.userRepo.createRecord(userInput);
-          }
+        username = cognitoResp?.UserSub;
+      } catch (error) {
+        console.log('Cognito Super Admin User Creation Error: ', error);
+        try {
+          cognitoResp =
+            await this.cognitoService.getUserFromCognito(adminEmailId);
+          username = cognitoResp.Username;
+        } catch (err) {
+          console.log('Cognito Super Admin User Fetch Error: ', err);
         }
-        return adminUser;
       }
-
-      public async getUserByUserName(userName: string): Promise<User> {
-        const user = await this.userRepo
-          .createQueryBuilder('user')
-          .leftJoinAndSelect('user.members', 'members')
-          .leftJoin('user.client', 'client')
-          .addSelect([
-            'client.id',
-            'client.name',
-            'client.isActive',
-            'client.clientBusinessType',
-            'client.isEdiEnrollmentRequired',
-          ])
-          .where('user.username = :userName', {
-            userName,
-          })
-          .getOne();
-    
-        if (!user) {
-          throw new UnauthenticatedException(`User not exist`);
-        } else if (
-          user?.userRole !== UserRole.SUPER_ADMIN 
-        ) {
-          if (!user?.isActive) {
-            throw new UnauthenticatedException('User is Disabled');
-          }
-        }
-        return user;
+      if (username) {
+        const userInput: any = {
+          username,
+          emailId: adminEmailId,
+          firstName: SUPERADMIN_FIRST_NAME,
+          lastName: SUPERADMIN_LAST_NAME,
+          isActive: true,
+          userRole: UserRole.SUPER_ADMIN,
+        };
+        adminUser = await this.userRepo.createRecord(userInput);
       }
+    }
+    return adminUser;
+  }
 
-      
+  public async getUserByUserName(userName: string): Promise<User> {
+    const user = await this.userRepo
+      .createQueryBuilder('user')
+      .where('user.username = :userName', {
+        userName,
+      })
+      .getOne();
+    if (!user) {
+      throw new UnauthenticatedException(`User not exist`);
+    } else if (user?.userRole !== UserRole.SUPER_ADMIN) {
+      if (!user?.isActive) {
+        throw new UnauthenticatedException('User is Disabled');
+      }
+    }
+    return user;
+  }
+
   public async getUserByEmailId(emailId: string): Promise<User> {
-    const user = await this.userRepo.findOneByCondRel(
-      { emailId },
-      {
-        createdBy: true,
-      },
-    );
+    const user = await this.userRepo
+      .createQueryBuilder('user')
+      .select('user')
+      .where('user.emailId = :emailId', { emailId })
+      .getOne();
     if (!user) {
       throw new UnauthenticatedException(`User not exist`);
     }
     return user;
   }
 
-      public async singIn(emailId: string, password: string, isTPVAdminSignIn = false): Promise<string> {
-        await this.getUserByEmailId(emailId);
-    
-        return this.cognitoService.performAuth(emailId, password);
-      }
+  public async singIn(
+    emailId: string,
+    password: string,
+    isTPVAdminSignIn = false,
+  ): Promise<string> {
+    await this.getUserByEmailId(emailId);
+    return this.cognitoService.performAuth(emailId, password);
+  }
 }
