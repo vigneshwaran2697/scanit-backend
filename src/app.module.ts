b18589e5b7875284a42a2c7853f7e6dcef4f2045
vravic926_comcast <Vigneshwaran_Ravichandran@comcast.com>
2025-01-20 19:14:03 +0530

    FE vite port added

--- src/app.module.ts
+++ src/app.module.ts
@@ -52,7 +52,7 @@ export class AppModule implements NestModule {
     consumer
       .apply(
         cors({
-          origin: 'http://localhost:3000/',
+          origin: 'http://localhost:5173/',
         }),
       )
       .forRoutes('/*');
